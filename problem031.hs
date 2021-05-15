00275537a1479ba44cdb7890eebea6f2b892f6b3
Zac Crites <zac@criteshouse.com>
2021-05-15 11:39:10 -0600

    Alternative solution for Problem 31

--- problem031.hs
+++ problem031.hs
@@ -16,4 +16,21 @@ problem31 = 1 + length [
     200 == 1 * onep + 2 * twop + 5 * fivep + 10 * tenp + 20 * twentyp + 50 * fiftyp + 100 * oneP]
 
 
-main = do putStrLn $ show problem31
+-- One extra solution for a single £2 coin
+problem31' = 1 + countSolutions 0 100 where
+    countSolutions acc coinValue
+        | acc == 200 = 1  -- found a solution!
+        | acc >  200 = 0  -- not a solution, combined value is too high
+
+        -- Limit the coins used in finding additional solutions to those smaller than the current coin.
+        -- That is, if our current coin is 20p, don't bother trying to use 50p coins as those
+        -- will be tried later. Recursively add smaller and smaller coins until the value reaches
+        -- or exceeds 200p, then count the valid solutions. Once the number of current coin values
+        -- added exceeds 200p, we move onto the next coin value looking for a solution there.
+        | let getInnerSolutions nextCoinValue = countSolutions (acc + nextCoinValue) nextCoinValue,
+          otherwise = sum . map getInnerSolutions . filter (<=coinValue) $ [1, 2, 5, 10, 20, 50, 100]
+
+
+main = do
+    putStrLn $ show problem31
+    putStrLn $ show problem31'
